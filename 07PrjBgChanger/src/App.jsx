883855613c0c849c751594ad010f2edb4b36b8a5
nj099 <ajayjangira8955@gmail.com>
2024-07-09 10:56:09 +0530

    commit changes

---
+++ 07PrjBgChanger/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-0.5 rounded-lg mb-1">tailwind css</h1>
+    </>
+  )
+}
+
+export default App
